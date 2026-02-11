1515edbb31b58a16b4ecd23cb9bdafe1a8560c3b
Justin Ellis <justinellis@crossfitwillis.com>
2026-02-10 22:41:17 -0600

    Force dynamic overlay rendering

--- app/overlay/lower-third/page.tsx
+++ app/overlay/lower-third/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 export default function LowerThirdOverlay() {
   return <div style={{ padding: 16, color: "white" }}>Lower third overlay coming soon.</div>;
 }
